c30ff2ee4d49ba998c1a01c34c2005e9edeaa396
mk1329 <mk1329@dgu.ac.kr>
2023-08-09 21:29:34 +0900

    Feat: 배포 중 ...

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/'>
+  <BrowserRouter basename='likelion11_HW'>
     <App />
   </BrowserRouter>
 )
